08152353845c5cdf65e623507dfa2a2e5333c811
Ernest <okiyaernest54@gmail.com>
2024-07-26 08:54:06 -0700

    Initial Commit

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri July 26 11:15:54 2024
+
+@author: Ernest Okiya
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start a basic Flask web application"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Adding a specific route /hbnb"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
